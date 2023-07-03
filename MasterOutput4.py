3f15d16f8d011c62bbbc684cf144bc25e16560ac
Anele <ngcobo.anele@gmail.com>
2023-07-03 14:54:30 +0200

    Created a list of the campaigns >12

--- MasterOutput4.py
+++ MasterOutput4.py
@@ -5,8 +5,10 @@ import pandas as pd
 data1 = pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\CampaignData\output2.xlsx',sheet_name="X3")
 data2= pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\CampaignData\output3.xlsx',sheet_name="X3")
 
+lst = []
 for r in range(len(data1)):
     if data1.iloc[r,3] > 12:
-        print(data1.loc[r])
+       lst.append(data1.loc[r])
+print(lst)
         
     
