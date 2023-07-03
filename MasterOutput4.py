3436298bdd7fd0ec66408b498e56ca03433d58c3
Anele <ngcobo.anele@gmail.com>
2023-07-03 12:09:20 +0200

    Added excel files to merge

---
+++ MasterOutput4.py
@@ -0,0 +1,8 @@
+#Filter out campaigns with clicks greater than 12, using loop.
+
+import pandas as pd 
+
+data1 = pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\CampaignData\output2.xlsx',sheet_name="X3")
+data2= pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\CampaignData\output3.xlsx',sheet_name="X3")
+
+ 
